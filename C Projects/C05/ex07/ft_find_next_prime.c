b0e3987cb211f9d9cc8ca07df5ae3e505b541343
sudaban <sumutdaban@gmail.com>
2024-09-05 11:33:31 +0300

    42 Piscine's C Projects.
    August, 2024.

---
+++ C Projects/C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdaban <sdaban@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/31 18:22:12 by sdaban            #+#    #+#             */
+/*   Updated: 2024/08/31 18:29:09 by sdaban           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb < 2)
+		return (0);
+	i = 2;
+	while (i * i <= nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb < 2)
+		return (2);
+	while (nb >= 2)
+	{
+		if (ft_is_prime(nb))
+			return (nb);
+		nb++;
+	}
+	return (0);
+}
